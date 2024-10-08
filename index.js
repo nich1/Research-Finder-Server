cce50131c585372f7b6f1f3e507841bd03475fea
nich1 <91976504+nich1@users.noreply.github.com>
2024-10-08 11:29:29 -0500

    Test2

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Root route to test GET request
 app.get('/', (req, res) => {
   res.send('Hello, Express on Vercel!');
+});
+
+// POST route to echo back the request body
+app.post('/echo', (req, res) => {
+  res.json({
+    message: 'You sent the following data:',
+    data: req.body
+  });
 });
 
 app.listen(port, () => {
